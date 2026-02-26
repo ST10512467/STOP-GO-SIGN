982294cbc6d9d3ace83602c2551f15a966ac2ff0
ST10512467 <ST10512467@myemeris.edu.za>
2026-02-26 08:30:07 +0200

    STOP AND GO

---
+++ src/application/Application.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package application;
+
+/**
+ *
+ * @author user
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+
+//THE BELOW IS A DISPLAYED IMAGE OF A STOP AND GO SIGN USING JAVA WITH MAVEN
+    
+    public static void main(String[] args) {
+        System.out.println("  xxxxxxxxxxxx                     xxxxxxxxxxxx ");
+        System.out.println(" x             x                  x            x");
+        System.out.println("x               x                x              x");
+        System.out.println("x     STOP      x                x      GO      x");
+        System.out.println("x               x                x              x");
+        System.out.println(" x             x                  x            x");
+        System.out.println("  xxxxxxxxxxxx                     xxxxxxxxxxxx");
+        System.out.println("      xxxx                             xxxx");
+        System.out.println("      xxxx                             xxxx");
+        System.out.println("      xxxx                             xxxx");
+        System.out.println("      xxxx                             xxxx");
+        System.out.println("      xxxx                             xxxx");
+        System.out.println("      xxxx                             xxxx");
+        System.out.println("   xxxxxxxxxx                       xxxxxxxxxx");
+        System.out.println("   xxxxxxxxxx                       xxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxx                 xxxxxxxxxxxxxxxx");
+        System.out.println("xxxxxxxxxxxxxxxx                 xxxxxxxxxxxxxxxx");
+    }
+    
+}
